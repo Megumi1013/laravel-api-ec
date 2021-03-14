7fbe4406056d7593b50baf6c4aaf75337b9a7ddf
Megumi Miyatani <megumi.miyatani@gmail.com>
2021-03-14 15:57:01 +0900

    Initial commit

---
+++ shell/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# CONFIG VARIABLES
+# ----------------
+REPO_NAME="laravel-api-ec"
+MYSQL_ROOT_PASSWORD="secret"
+DATABASE_NAME="testdatabase"
+DATABASE_USER="megumimiyatani"
+DATABASE_PASSWORD="hsg7jJh793jks"
+
